ee8adc97bd9e4aa2896a9e5ebf6316c8a5e670aa
Ahsan <ahsanan@outlook.com>
2026-01-19 17:24:47 +0600

    Basic Implementation of .env & req Obj

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import http from 'http';
 
-const PORT = 9001;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    console.log('Method:', req.method);
+    console.log('URL:', req.url);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Server is running...\n');
     res.end('Hello World\n');
 })
