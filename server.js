d9a9c6ceb4e6cfbf5a57ad3b6287143329f1b8ac
Ahsan <ahsanan@outlook.com>
2026-01-19 23:34:16 +0600

    simple routing..

--- server.js
+++ server.js
@@ -4,11 +4,35 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
 
-    console.log('Method:', req.method);
-    console.log('URL:', req.url);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Server is running...\n');
-    res.end('Hello World\n');
+
+    try {
+
+        if (req.method === 'GET') {
+            if (req.url === '/') {
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('<h1>Welcome to the Home Page</h1>\n');
+            } else if (req.url === '/about') {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('<h1>About Page</h1>\n');
+            } else {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.end('<h1>Page Not Found</h1>\n');
+            }
+        } else {
+            throw new Error('Invalid Request Method');
+        }
+    } catch (err) {
+        res.writeHead(500, {
+            'Content-Type': 'text/html'
+        });
+        res.end(`<h1>Server Error: ${err.message}</h1>\n`);
+    }
 })
 
 server.listen(PORT, () => {
