1f8ecde202514f1806638fa6751812a052178543
Ahsan <ahsanan@outlook.com>
2026-01-20 19:14:11 +0600

    Load html files insead raw text

--- server.js
+++ server.js
@@ -9,40 +9,30 @@ const PORT = process.env.PORT || 5000;
 const  __filename = url.fileURLToPath(import.meta.url);
 const  __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
-
-
+const server = http.createServer(async (req, res) => {
     try {
-
         if (req.method === 'GET') {
-
             let filePath;
             if (req.url === '/') {
                 filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
                 filePath = path.join(__dirname, 'public', 'about.html');
-            }
-            else {
-             throw new Error('Invalid Request URL');
+            } else {
+                throw new Error('Invalid Request URL');
             }
 
-            const data = await fs.readFile(filePath,);
-            res.setHeader(
-                'Content-Type', 'text/html'
-            );
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
             res.write(data);
-
-            res.end
+            res.end();
         } else {
             throw new Error('Invalid Request Method');
         }
     } catch (err) {
-        res.writeHead(500, {
-            'Content-Type': 'text/html'
-        });
+        res.writeHead(500, { 'Content-Type': 'text/html' });
         res.end(`<h1>Server Error: ${err.message}</h1>\n`);
     }
-})
+});
 
 server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
