53fbed901de08ac43b56d5e4eeb5e2e9b272012e
Ahsan <ahsanan@outlook.com>
2026-01-20 19:14:00 +0600

    Load html files insead raw text

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 import http from 'http';
-
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT || 5000;
+
+// Get current path
+
+const  __filename = url.fileURLToPath(import.meta.url);
+const  __dirname = path.dirname(__filename);
 
 const server = http.createServer((req, res) => {
 
@@ -8,22 +15,24 @@ const server = http.createServer((req, res) => {
     try {
 
         if (req.method === 'GET') {
+
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                res.end('<h1>Welcome to the Home Page</h1>\n');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-                res.end('<h1>About Page</h1>\n');
-            } else {
-                res.writeHead(404, {
-                    'Content-Type': 'text/html'
-                });
-                res.end('<h1>Page Not Found</h1>\n');
+                filePath = path.join(__dirname, 'public', 'about.html');
             }
+            else {
+             throw new Error('Invalid Request URL');
+            }
+
+            const data = await fs.readFile(filePath,);
+            res.setHeader(
+                'Content-Type', 'text/html'
+            );
+            res.write(data);
+
+            res.end
         } else {
             throw new Error('Invalid Request Method');
         }
